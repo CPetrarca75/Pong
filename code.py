79e35a7c28caae9f23d902a6a3ed280d5c9f9d7c
CPetrarca75 <114268788+CPetrarca75@users.noreply.github.com>
2023-03-15 03:11:06 -0400

    Update code.py

--- code.py
+++ code.py
@@ -14,6 +14,7 @@ Size_X = 400
 Size_Y = 400
 Score1 = 0
 Score2 = 0
+game_state = 0
 
 
 #Window Init
@@ -90,10 +91,14 @@ def pscore(i,j):
                 pygame.draw.rect(window,white,(300,25,5,15))
                 pygame.draw.rect(window,white,(300,40,20,5))
                 pygame.draw.rect(window,white,(320,25,5,35))
-
+#Endgame Screen
 def endgame():
+    global game_state
+    global Score1
+    global Score2
     window.fill(black)
     pygame.display.update()
+    pygame.time.delay(5)
     
     #P
     pygame.draw.rect(window,white,(100,35,5,35))  
@@ -134,240 +139,283 @@ def endgame():
     pygame.draw.rect(window,white,(280,40,5,10))
     pygame.draw.rect(window,white,(300,55,5,10))
 
+    pygame.display.update()
+    pygame.time.delay(1000)
+    pygame.display.update()
+
+    restart_screen()
+    
+
 def cont():
+    global game_state
+    # game_state = 0
+    # return
+    global game_state
+    global Score1
+    global Score2
     flash  = 0
     select = 1  
     blink = True
     while blink:
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                game_state = 2
+                return
         keys = pygame.key.get_pressed()
+        pygame.event.pump() #Necessary to register key presses
         if select == 0:
-            if flash >= 0:
-                if keys[pygame.K_SPACE]:
-                    return True
-            #     if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-            #         select = 1
-            #     if flash == 2000:
-            #         flash *= -1
-            #     pygame.draw.rect(window,white,(70,160,257,55),2)
-            #     flash +=1
-            #     pygame.display.update()
-            # if flash < 0:
-            #     if keys[pygame.K_SPACE]:
-            #         return True
-            #     if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-            #         select = 1
-            #     pygame.draw.rect(window,black,(70,160,257,55),2)
-            #     flash +=1
-            #     pygame.display.update()
+            pygame.draw.rect(window,white,(140,240,127,55),2)
+            if keys[pygame.K_SPACE]:
+                    game_state = 0
+                    Score1 = 0
+                    Score2 = 0
+                    return
+            if keys[pygame.K_DOWN]:
+                    select = 1
+                    #pygame.time.delay(1)
+                    #pygame.display.update()
+                    flash = 0
+                    continue
+            if flash >= 0:    
+                if flash == 2000:
+                    flash *= -1
+                pygame.draw.rect(window,white,(70,160,257,55),2)
+                flash +=1
+                pygame.display.update()
+            if flash < 0:
+                pygame.draw.rect(window,black,(70,160,257,55),2)
+                flash +=1
+                pygame.display.update()
 
         if select == 1:
+            pygame.draw.rect(window,white,(70,160,257,55),2)
+            if keys[pygame.K_SPACE]:
+                    game_state = 2 
+                    return
+            if keys[pygame.K_UP]:
+                    select = 0
+                    #pygame.time.delay(1)
+                    pygame.display.update()
+                    flash = 0
+                    continue
             if flash >= 0:
-                if keys[pygame.K_SPACE]:
-                    return False
-            #     if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-            #         select = 0
-            #     if flash == 2000:
-            #         flash *= -1
-            #     pygame.draw.rect(window,white,(140,240,127,55),2)
-            #     flash +=1
-            #     pygame.display.update()
-            # if flash < 0:
-            #     if keys[pygame.K_SPACE]:
-            #         return False
-            #     if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-            #         select = 0
-            #     pygame.draw.rect(window,black,(140,240,127,55),2)
-            #     flash +=1
-            #     pygame.display.update()
+                if flash == 2000:
+                    flash *= -1
+                pygame.draw.rect(window,white,(140,240,127,55),2)
+                flash +=1
+                pygame.display.update()
+            if flash < 0:
+                pygame.draw.rect(window,black,(140,240,127,55),2)
+                flash +=1
+                pygame.display.update()
+            
+                
+
 
 
 def restart_screen():
-    check = True
-    while(check):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
-                return False
-        #Restart
-        #R
-        pygame.draw.rect(window,white,(80,170,5,35))
-        pygame.draw.rect(window,white,(80,170,15,5))
-        pygame.draw.rect(window,white,(95,175,5,10))
-        i = 0
-        j=0
-        while j < 4:
-            pygame.draw.rect(window,white,(80+i,185+i,5,5))
-            i+=5
-            j+=1
-        pygame.draw.rect(window,white,(80,185,15,5))
-        #E
-        pygame.draw.rect(window,white,(110,170,5,30))
-        pygame.draw.rect(window,white,(110,170,20,5))
-        pygame.draw.rect(window,white,(110,185,20,5))
-        pygame.draw.rect(window,white,(110,200,20,5))
-        #S
-        pygame.draw.rect(window,white,(145,170,20,5))
-        pygame.draw.rect(window,white,(145,185,15,5))
-        pygame.draw.rect(window,white,(140,200,20,5))
-        pygame.draw.rect(window,white,(140,175,5,10))
-        pygame.draw.rect(window,white,(160,190,5,10))
-        #T
-        pygame.draw.rect(window,white,(175,170,30,5))
-        pygame.draw.rect(window,white,(187,175,6,30))
-        #A
-        pygame.draw.rect(window,white,(215,175,5,30))
-        pygame.draw.rect(window,white,(240,175,5,30))
-        pygame.draw.rect(window,white,(220,170,20,5))
-        pygame.draw.rect(window,white,(220,185,20,5))
-        #R
-        pygame.draw.rect(window,white,(255,170,5,35))
-        pygame.draw.rect(window,white,(255,170,15,5))
-        pygame.draw.rect(window,white,(270,175,5,10))
-        i = 0
-        j=0
-        while j < 4:
-            pygame.draw.rect(window,white,(255+i,185+i,5,5))
-            i+=5
-            j+=1
-        pygame.draw.rect(window,white,(255,185,15,5))
-        #T
-        pygame.draw.rect(window,white,(285,170,30,5))
-        pygame.draw.rect(window,white,(297,175,6,30))
-        #Box        
-        pygame.draw.rect(window,white,(70,160,257,55),2)
-        
-        
-        #Exit
+    global game_state
+    
+    #Restart
+    #R
+    pygame.draw.rect(window,white,(80,170,5,35))
+    pygame.draw.rect(window,white,(80,170,15,5))
+    pygame.draw.rect(window,white,(95,175,5,10))
+    i = 0
+    j=0
+    while j < 4:
+        pygame.draw.rect(window,white,(80+i,185+i,5,5))
+        i+=5
+        j+=1
+    pygame.draw.rect(window,white,(80,185,15,5))
+    #E
+    pygame.draw.rect(window,white,(110,170,5,30))
+    pygame.draw.rect(window,white,(110,170,20,5))
+    pygame.draw.rect(window,white,(110,185,20,5))
+    pygame.draw.rect(window,white,(110,200,20,5))
+    #S
+    pygame.draw.rect(window,white,(145,170,20,5))
+    pygame.draw.rect(window,white,(145,185,15,5))
+    pygame.draw.rect(window,white,(140,200,20,5))
+    pygame.draw.rect(window,white,(140,175,5,10))
+    pygame.draw.rect(window,white,(160,190,5,10))
+    #T
+    pygame.draw.rect(window,white,(175,170,30,5))
+    pygame.draw.rect(window,white,(187,175,6,30))
+    #A
+    pygame.draw.rect(window,white,(215,175,5,30))
+    pygame.draw.rect(window,white,(240,175,5,30))
+    pygame.draw.rect(window,white,(220,170,20,5))
+    pygame.draw.rect(window,white,(220,185,20,5))
+    #R
+    pygame.draw.rect(window,white,(255,170,5,35))
+    pygame.draw.rect(window,white,(255,170,15,5))
+    pygame.draw.rect(window,white,(270,175,5,10))
+    i = 0
+    j=0
+    while j < 4:
+        pygame.draw.rect(window,white,(255+i,185+i,5,5))
+        i+=5
+        j+=1
+    pygame.draw.rect(window,white,(255,185,15,5))
+    #T
+    pygame.draw.rect(window,white,(285,170,30,5))
+    pygame.draw.rect(window,white,(297,175,6,30))
+    #Box        
+    pygame.draw.rect(window,white,(70,160,257,55),2)
+    
+    
+    #Exit
 
-        #E
-        pygame.draw.rect(window,white,(150,250,5,30))
-        pygame.draw.rect(window,white,(150,250,20,5))
-        pygame.draw.rect(window,white,(150,265,20,5))
-        pygame.draw.rect(window,white,(150,280,20,5))
-        #X
-        pygame.draw.rect(window,white,(180,250,5,10))
-        pygame.draw.rect(window,white,(180,280,5,5))
-        pygame.draw.rect(window,white,(200,250,5,10))
-        pygame.draw.rect(window,white,(200,280,5,5))
-        x_c = 0
-        x = 0
-        while x_c < 4:
-            pygame.draw.rect(window,white,(185+x,260+x,5,5))
-            pygame.draw.rect(window,white,(195-x,260+x,5,5))
-            x += 5
-            x_c += 1
-        #I
-        pygame.draw.rect(window,white,(215,250,5,35))
-        #T
-        pygame.draw.rect(window,white,(230,250,30,5))
-        pygame.draw.rect(window,white,(242,255,6,30))
-        #Border
-        pygame.draw.rect(window,white,(140,240,127,55),2)
-        pygame.display.update() 
+    #E
+    pygame.draw.rect(window,white,(150,250,5,30))
+    pygame.draw.rect(window,white,(150,250,20,5))
+    pygame.draw.rect(window,white,(150,265,20,5))
+    pygame.draw.rect(window,white,(150,280,20,5))
+    #X
+    pygame.draw.rect(window,white,(180,250,5,10))
+    pygame.draw.rect(window,white,(180,280,5,5))
+    pygame.draw.rect(window,white,(200,250,5,10))
+    pygame.draw.rect(window,white,(200,280,5,5))
+    x_c = 0
+    x = 0
+    while x_c < 4:
+        pygame.draw.rect(window,white,(185+x,260+x,5,5))
+        pygame.draw.rect(window,white,(195-x,260+x,5,5))
+        x += 5
+        x_c += 1
+    #I
+    pygame.draw.rect(window,white,(215,250,5,35))
+    #T
+    pygame.draw.rect(window,white,(230,250,30,5))
+    pygame.draw.rect(window,white,(242,255,6,30))
+    #Border
+    pygame.draw.rect(window,white,(140,240,127,55),2)
+    pygame.display.update() 
+    cont()
+    
 
-        return(cont())
+
+        
             
 
-running  = True
-while running:
-    pygame.time.delay(10)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            running = False
+def main_game():
 
     #help move objects
+    #Object Creation
+    #Barrier Init
+    global BarrierSize_Width 
+    global BarrierSize_Length 
+    global B1_X_Coord 
+    global B1_Y_Coord 
+    global B2_X_Coord 
+    global B2_Y_Coord
+    global barrier_velo
+    global B1_Color 
+    global B2_Color 
+    global Score1
+    global Score2
+    global game_state
+
+        #Ball Init
+    global BallSize
+    global Ball_X_Coord 
+    global Ball_Y_Coord 
+    global ball_xvelo
+    global ball_yvelo
+    global base_velo
     
-    Ball_X_Coord += ball_xvelo
-    Ball_Y_Coord += ball_yvelo
-    ball_xvelo *= 1.0005
-    ball_yvelo *= 1.0005
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] and B2_Y_Coord > 0:
-        B2_Y_Coord -= barrier_velo
-    
-    if keys[pygame.K_w] and B1_Y_Coord > 0:
-        B1_Y_Coord -= barrier_velo
-
-    if keys[pygame.K_DOWN] and B2_Y_Coord < Size_Y-BarrierSize_Length:
-        B2_Y_Coord += barrier_velo
-
-    if keys[pygame.K_s] and B1_Y_Coord < Size_Y-BarrierSize_Length:
-        B1_Y_Coord += barrier_velo
-
-    
-
-
-    
-
-
-
-    #Need to Continually Fill back the space as thepreviously created drawing stays
-    window.fill((0,0,0))
-
-    #Initial Creation of Objects
-    Barrier_1 = pygame.draw.rect(window,B1_Color,(B1_X_Coord,B1_Y_Coord,BarrierSize_Width,BarrierSize_Length))
-    Barrier_2 = pygame.draw.rect(window,B2_Color,(B2_X_Coord,B2_Y_Coord,BarrierSize_Width,BarrierSize_Length))
-    Ball = pygame.draw.rect(window,white,(Ball_X_Coord,Ball_Y_Coord,BallSize,BallSize))
-
-    #Ball Collision
-    Ball_Collide = Ball.colliderect(Barrier_1) or Ball.colliderect(Barrier_2)
-
-    
-
-            
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                game_state = 2
+                return
+        pygame.time.delay(10) #Necesary to keep game at stable pace
+        Ball_X_Coord += ball_xvelo
+        Ball_Y_Coord += ball_yvelo
+        ball_xvelo *= 1.0005
+        ball_yvelo *= 1.0005
         
+        keys = pygame.key.get_pressed()
+        pygame.event.pump() #Necessary to register key presses
+        if keys[pygame.K_UP] and B2_Y_Coord > 0:
+            B2_Y_Coord -= barrier_velo
         
-            
+        if keys[pygame.K_w] and B1_Y_Coord > 0:
+            B1_Y_Coord -= barrier_velo
 
-    #Ball Restart:
-    if Ball_X_Coord < -25  or Ball_X_Coord + BallSize > 425:
-        if Ball_X_Coord < -25:
-            Score2 += 1 #Right Side
-        else:
-            Score1 += 1 #Right Side
-        Ball_X_Coord = 190
-        Ball_Y_Coord = 190
-        pygame.time.wait(100)
-        pygame.display.update()
-        ball_yvelo = base_velo * choice([-1,1])
-        ball_xvelo = base_velo * choice([-1,1])
-        pscore(0,Score1) #Updating player 1 score (Left Side)
-        pscore(1,Score2) #Updating Player 2 score (Right Side)
-        pygame.display.update()
-        #CREATE A SECOND BOX TAHT IS ONLY VISIBILE WHILE THE PAUSE OCCURS THEN REMOVE IT WHEN PAUSE ENDS 
-        if Score1 == 5 or Score2 == 5:
-            endgame()
-            pygame.display.update()
-            pygame.time.delay(3000)
-            pygame.display.update()
-            if restart_screen() == False:
-                running = False
+        if keys[pygame.K_DOWN] and B2_Y_Coord < Size_Y-BarrierSize_Length:
+            B2_Y_Coord += barrier_velo
+
+        if keys[pygame.K_s] and B1_Y_Coord < Size_Y-BarrierSize_Length:
+            B1_Y_Coord += barrier_velo
+
+        #Need to Continually Fill back the space as thepreviously created drawing stays
+        window.fill((0,0,0))
+
+        #Initial Creation of Objects
+        Barrier_1 = pygame.draw.rect(window,B1_Color,(B1_X_Coord,B1_Y_Coord,BarrierSize_Width,BarrierSize_Length))
+        Barrier_2 = pygame.draw.rect(window,B2_Color,(B2_X_Coord,B2_Y_Coord,BarrierSize_Width,BarrierSize_Length))
+        Ball = pygame.draw.rect(window,white,(Ball_X_Coord,Ball_Y_Coord,BallSize,BallSize))
+
+        #Ball Collision
+        Ball_Collide = Ball.colliderect(Barrier_1) or Ball.colliderect(Barrier_2)
+
+        #Ball Restart:
+        if Ball_X_Coord < -25  or Ball_X_Coord + BallSize > 425:
+            if Ball_X_Coord < -25:
+                Score2 += 1 #Right Side
             else:
-                Score1 = 0
-                Score2 = 0
+                Score1 += 1 #Right Side
+            Ball_X_Coord = 190
+            Ball_Y_Coord = 190
+            pygame.time.wait(100)
+            pygame.display.update()
+            ball_yvelo = base_velo * choice([-1,1])
+            ball_xvelo = base_velo * choice([-1,1])
+            pscore(0,Score1) #Updating player 1 score (Left Side)
+            pscore(1,Score2) #Updating Player 2 score (Right Side)
+            pygame.display.update()
+            #CREATE A SECOND BOX TAHT IS ONLY VISIBILE WHILE THE PAUSE OCCURS THEN REMOVE IT WHEN PAUSE ENDS 
+            if Score1 == 5 or Score2 == 5:
+                game_state = 1
+                return
+
+        if(Ball_X_Coord == 190 and Ball_Y_Coord == 190 and (Score1+Score2 >=0)):
+            pygame.time.wait(500)
             
-        pygame.display.update()
+        #Ball Collision in Play    
+        if Ball_Collide:
+                if Ball_X_Coord < 380 or Ball_X_Coord > 20: 
+                    ball_xvelo = -ball_xvelo
+                    if abs(ball_xvelo) > 3.5:
+                        ball_xvelo = ball_xvelo * choice([.6,.8,.9,1.01,1.05,1.3])
+                        ball_yvelo = ball_yvelo * choice([.5,.75,1,1.25,1.5]) 
+                    else:
+                        ball_xvelo = ball_xvelo * choice([1,1.05,1.35,1.5])
+                        ball_yvelo = ball_yvelo * choice([1,1.05,1.35,1.5])
+                    
+                
         
-    
-    if(Ball_X_Coord == 190 and Ball_Y_Coord == 190 and (Score1+Score2 >=0)):
-        pygame.time.wait(500)
-        
-    #Ball Collision in Play    
-    if Ball_Collide:
-            if Ball_X_Coord < 380 or Ball_X_Coord > 20: 
-                ball_xvelo = -ball_xvelo
-                if abs(ball_xvelo) > 3.5:
-                    ball_xvelo = ball_xvelo * choice([.6,.8,.9,1.01,1.05,1.3])
-                    ball_yvelo = ball_yvelo * choice([.5,.75,1,1.25,1.5]) 
-                else:
-                    ball_xvelo = ball_xvelo * choice([1,1.05,1.5])
-                    ball_yvelo = ball_yvelo * choice([1,1.05,1.5])
-                #!!!If you want to change the velo more track a timer on how long buttons are held to move barriers
-            
-    
-    if Ball_Y_Coord > 400 - BallSize or Ball_Y_Coord < 0:
-        ball_yvelo = -ball_yvelo
+        if Ball_Y_Coord > 400 - BallSize or Ball_Y_Coord < 0:
+            ball_yvelo = -ball_yvelo
 
 
 #NEED TO ADJUST THE X COLLIDE OF THE BALL
-    pygame.display.update()
+        pygame.display.update()
+
+running  = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            running = False
+    if game_state == 0:
+        main_game()
+    elif game_state == 1:
+        endgame()
+    else:
+        running = False
+
+
+
